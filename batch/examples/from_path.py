06c3ada9e639ae6a9aee64cbb63676cf6e5e5bba
kingstar535 <wangxing1217@126.com>
2020-07-29 04:22:39 -0400

    upload file

---
+++ batch/examples/from_path.py
@@ -0,0 +1,36 @@
+# CsvTableSource读取csv文件并定义Schema
+# register_table_source将CsvTableSource注册成数据库中的Table
+# from_path读取数据库中指定的Table，生成table
+import time
+import os
+from pyflink.table import CsvTableSource, CsvTableSink
+from pyflink.table.descriptors import FileSystem, OldCsv, Schema
+from pyflink.table.types import DataTypes
+from env import BT_ENV
+
+print(BT_ENV.list_databases())
+
+
+source_file = os.getcwd() + "/input/nba_players.csv"
+print(source_file)
+
+
+BT_ENV.register_table_source("nba_player",
+                             CsvTableSource(source_file,
+                                            ["season", "name", "apperances", "starts", \
+                                             "minutes", "assist", "steal", "block", "score"],
+                                            [DataTypes.STRING(),
+                                             DataTypes.STRING(),
+                                             DataTypes.INT(),
+                                             DataTypes.INT(),
+                                             DataTypes.FLOAT(),
+                                             DataTypes.FLOAT(),
+                                             DataTypes.FLOAT(),
+                                             DataTypes.FLOAT(),
+                                             DataTypes.FLOAT()]))
+
+print(BT_ENV.list_tables())
+t1 = BT_ENV.from_path("nba_player") # scan source table
+print(type(t1)) # <class 'pyflink.table.table.Table'>
+# 打印内容
+t1.execute().print()
